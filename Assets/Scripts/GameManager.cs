bc75d53e5edad601898d70fb3e439f08c02acbd8
J-bit812 <142627732+J-bit812@users.noreply.github.com>
2024-02-28 12:58:41 -0800

    Lesson 5.3

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Button restartButton;
+
+    public TextMeshProUGUI gameOverText;
+
     public static GameManager instance;
 
     public List<GameObject> targets;
@@ -15,6 +21,8 @@ public class GameManager : MonoBehaviour
 
     public TextMeshProUGUI scoreText;
 
+    public bool isGameActive {get; private set;}
+
     private void Awake()
     {
         instance = this;
@@ -22,10 +30,24 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        isGameActive = true;
+        gameOverText.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(false);
         StartCoroutine(SpawnTarget());
         UpdateScore(0);
     }
 
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
     public void UpdateScore( int scoreToAdd)
     {
         score += scoreToAdd;
@@ -35,7 +57,7 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -35,6 +35,7 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
+       if (!GameManager.instance.isGameActive) return;
         Destroy(gameObject);
         GameManager.instance.UpdateScore(pointValue);
         Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
@@ -43,6 +44,10 @@ public class Target : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if(!gameObject.CompareTag("Bad"))
+        {
+            GameManager.instance.GameOver();
+        }
     }
 
     Vector3 RandomForce()
